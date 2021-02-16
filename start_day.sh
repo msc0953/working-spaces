b4b7cf9e19e9782b9eb3520d968546ee73c6d7f6
wclee <msc0953@gmail.com>
2021-02-17 01:41:49 +0800

    update start_day

--- start_day.sh
+++ start_day.sh
@@ -5,20 +5,21 @@
 #| (_) \__ \>  <    | | | | \__ \ || (_| | | |
 # \___/|___/_/\_\   |_|_| |_|___/\__\__,_|_|_|
 
-echo "I  ❤️  🍎"
+echo "I ❤️ 🍎"
 echo "Good Morning!!"
 echo "By Vans Li"
 echo "Follow me on twitter! https://twitter.com/msc0953"
 
 # Open My Project
+echo "Open Project by Pycharm"
 charm /Users/wclee/workspace/test-data-microservice
 
 # Open Applications
 open -a "Bear"
-open -a "Google Chrome"
 open -a "Google Chrome" "https://dev.azure.com/apexcentral-auto-trainning/"
 open -a "Google Chrome" "https://quii.gitbook.io/learn-go-with-tests/"
 
 # Spotify
 open -a Spotify
 osascript -e 'tell application "Spotify" to play'
+
