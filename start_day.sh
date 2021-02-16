c399a60e1adfb2963ba486757f339c88f708ebcd
wclee <msc0953@gmail.com>
2021-02-17 00:51:02 +0800

    add start_day.sh

---
+++ start_day.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo ""
+echo "By Vans Li"
+echo "Follow me on twitter! https://twitter.com/msc0953"
+
+charm /users/wclee/workspace/test-data-microservice
+open -a Bear
+open -a Spotify
