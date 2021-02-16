d9516792ee8bc573346abc47debcae8ad9d3f9cd
wclee <msc0953@gmail.com>
2021-02-17 01:28:23 +0800

    add setup.sh to ref

--- start_day.sh
+++ start_day.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
-echo ""
+#                    _           _        _ _ 
+#  ___  _____  __   (_)_ __  ___| |_ __ _| | |
+# / _ \/ __\ \/ /   | | '_ \/ __| __/ _` | | |
+#| (_) \__ \>  <    | | | | \__ \ || (_| | | |
+# \___/|___/_/\_\   |_|_| |_|___/\__\__,_|_|_|
+
+echo "I  ❤️  🍎"
+echo "Good Morning!!"
 echo "By Vans Li"
 echo "Follow me on twitter! https://twitter.com/msc0953"
 
-charm /users/wclee/workspace/test-data-microservice
-open -a Bear
+# Open My Project
+charm /Users/wclee/workspace/test-data-microservice
+
+# Open Applications
+open -a "Bear"
+open -a "Google Chrome"
+
+# Spotify
 open -a Spotify
+osascript -e 'tell application "Spotify" to play'
